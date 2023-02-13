8c5e477e0ba567a3a0f7ef644f5e6d483ce9b38e
Ellisvelandia <eyis619l@gmail.com>
2023-02-12 19:02:13 -0500

    youtube

--- app.js
+++ app.js
@@ -4,45 +4,50 @@ let api_key = "AIzaSyCfpfvYnkY_8Vkxp3AHaXumDJSoH6nKeyk";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
-fetch(
-  video_http +
-    new URLSearchParams({
-      key: api_key,
-      part: "snippet",
-      chart: "mostPopular",
-      maxResults: 50,
-      regionCode: "CO",
-    })
-)
-  .then((res) => res.json())
-  .then((data) => {
-    data.items.forEach(item => {
+async function getVideos() {
+  try {
+    const res = await fetch(
+      video_http +
+        new URLSearchParams({
+          key: api_key,
+          part: "snippet",
+          chart: "mostPopular",
+          maxResults: 10,
+          regionCode: "CO",
+        })
+    );
+    const data = await res.json();
+    data.items.forEach((item) => {
       getChannelIcon(item);
     });
-  })
-  .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-const getChannelIcon = (video_data) => {
-  fetch(
-    channel_http +
-      new URLSearchParams({
-        key: api_key,
-        part: "snippet",
-        id: video_data.snippet.channelId,
-      })
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      video_data.channelThumbnail =
-        data.items[0].snippet.thumbnails.default.url;
-      makeVideoCard(video_data);
-    });
+const getChannelIcon = async (video_data) => {
+  try {
+    const res = await fetch(
+      channel_http +
+        new URLSearchParams({
+          key: api_key,
+          part: "snippet",
+          id: video_data.snippet.channelId,
+        })
+    );
+    const data = await res.json();
+    video_data.channelThumbnail =
+      data.items[0].snippet.thumbnails.default.url;
+    makeVideoCard(video_data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const makeVideoCard = (data) => {
   videoContainer.innerHTML += `
   <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
-    <img src="${data.snippet.thumbnails.high.url}" alt="" class="thumbnail">
+  <iframe class="thumbnail" width="560" height="315" src="https://www.youtube.com/embed/${data.id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in- picture; web-share" allowfullscreen load="lazy"></iframe>
     <div class="content">
       <img src="${data.channelThumbnail}" alt="" class="channel-icon">
       <div class="info">
@@ -62,3 +67,5 @@ searchBtn.addEventListener("click", () => {
     location.href = searchLink + searchInut.value;
   }
 });
+
+getVideos();
